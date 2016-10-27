390c857949566228d92b7701b3a39510795c899c
Jessica Yeung <jjyeung@ucsd.edu>
2016-10-26 19:40:55 -0700

    Fixed the header and added skeletons

--- static/js/main.js
+++ static/js/main.js
@@ -14,13 +14,3 @@ $( ".cross" ).click(function() {
 		$( ".hamburger" ).show();
 	});
 });
-
-$('head style[type="text/css"]').attr('type', 'text/less');
-less.refreshStyles();
-window.randomize = function() {
-	$('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
-}
-setTimeout(window.randomize, 200);
-$('.radial-progress').click(window.randomize);
-
-// Read more here: https://medium.com/@andsens/radial-progress-indicator-using-css-a917b80c43f9
