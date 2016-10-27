9b25beb6e108cbf2cc18deeac4b0a8104bd56c45
Jessica Yeung <jjyeung@ucsd.edu>
2016-10-26 18:26:40 -0700

    Added task page skeleton

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,26 @@
 $( ".cross" ).hide();
 $( ".menu" ).hide();
+
 $( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
+	$( ".menu" ).slideToggle( "slow", function() {
+		$( ".hamburger" ).hide();
+		$( ".cross" ).show();
+	});
 });
 
 $( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
+	$( ".menu" ).slideToggle( "slow", function() {
+		$( ".cross" ).hide();
+		$( ".hamburger" ).show();
+	});
 });
-});
+
+$('head style[type="text/css"]').attr('type', 'text/less');
+less.refreshStyles();
+window.randomize = function() {
+	$('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
+}
+setTimeout(window.randomize, 200);
+$('.radial-progress').click(window.randomize);
+
+// Read more here: https://medium.com/@andsens/radial-progress-indicator-using-css-a917b80c43f9
