a924fdf52b9b883d84c7f100b49ad13e3f1d6836
Mengying Li <mel116@ucsd.edu>
2016-11-10 19:02:02 -0800

    if we can get the submit new task wokrking then it's done

--- routes/task.js
+++ routes/task.js
@@ -1,20 +1,5 @@
-//! 1. open ../data.json
-var database = require('../data.json');
-console.log(database);
-
-
-//! 2. add the new task to the database
-console.log(req.query);// print the form 
-???????database["users"][0]["assignments"].push ….
-database.push({ 'taskName: req.query.task_name, 'dueDate':req….});
-
-
-//! 3. save the database
-i fucking dont know the code
-how to open a file?
-how to write into a file
-open => ../data.json
-write into that file "database" variable
+// Get all of our user data
+var data = require('../data.json');
 
 exports.view = function(req, res){
 	//console.log(data);
