bf06b9223aec7833da16172899cae9810eb7a18b
Mengying Li <mel116@ucsd.edu>
2016-11-11 02:06:10 -0800

    still buggy...

--- routes/submit_new_task.js
+++ routes/submit_new_task.js
@@ -1,5 +1,5 @@
 // Get all of our user data
-var data = require('../data.json');
+//var data = require('../data.json');
 
 exports.view = function(req, res){
 
@@ -10,21 +10,23 @@ console.log(database);
 
 //! 2. add the new task to the database
 console.log(req.query);//print the form 
-{{#with users.[0]}}
-				{{#each assignments}}
-database["users"][0]["assignments"].push ….
-database.push({ 'taskName: req.query.task_name, 'dueDate':req…. , 'timeDue': ... });
+
+//database.users.push()
+newTask = {'task': req.query.taskname, 'due_date': req.query.calendar};
+database["users"][0]["assignments"].push({'task': req.query.taskname, 'due_date': req.query.calendar});
+//database.push({ 'taskName': task, 'dueDate': due_date});
+console.log({'task': req.query.taskname, 'due_date': req.query.calendar});
 
 
 //! 3. save the database
-i fucking dont know the code
-how to open a file?
-how to write into a file
-open => ../data.json
-write into that file "database" variable
+//i fucking dont know the code
+//how to open a file?
+//how to write into a file
+//open => ../data.json
+//write into that file "database" variable
 
 
 
 	//console.log(data);
-	res.render('submit_new_task', data);
+	res.render('submit_new_task', database);
 }
