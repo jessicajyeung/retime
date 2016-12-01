693e5b86eb78d185e81568455698e35a6a30d83b
Jessica Yeung <jjyeung@ucsd.edu>
2016-12-01 10:57:39 -0800

    catching up

--- routes/task.js
+++ routes/task.js
@@ -2,11 +2,5 @@
 var data = require('../data.json');
 
 exports.view = function(req, res) {
-	// controller code goes here
-	var task = req.params.name;
-	console.log('The taskname is: ' + task);
-	res.render('task', {
-		'taskname': task
-	});
-	res.render('task');
+	res.render('task', data["users"][0]["assignments"][parseInt(req.query.index)]);
 }
